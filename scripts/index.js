f4ae737f9ae3b7ac298265ec94a833a040b78752
dlb <dlb96@qq.com>
2018-03-09 21:22:37 +0800

    change style of index

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
-var mid = new Vue({
-    el: '#mid',
+var name = new Vue({
+    el: '#name',
     data: {
         massage : "Snap's Blog"
     },
     methods: {
         jumpList: function () {
-            window.location.href="./htmls/list.html"
+            window.location.href='./htmls/list.html'
         }
     }
 })
