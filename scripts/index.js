cc59d5175d797a4f1dc39eb097d2bd6289b2a4d4
dlb <dlb96@qq.com>
2018-03-09 17:18:16 +0800

    list page added

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+window.onload(void function(){
+    var app5 = new Vue({
+        el: '#blogname',
+        methods: {
+          jumpList: function () {
+            window.location.href="htmls/list.html"
+          }
+        }
+      })
+})
