c9adb4c2b9c7ba98b6415135aba1fb210069d35d
dlb <dlb96@qq.com>
2018-03-12 20:56:06 +0800

    004 xifen

--- scripts/list.js
+++ scripts/list.js
@@ -4,7 +4,8 @@ var app4 = new Vue({
       blogs: [
         { title: '刚开始写python遇到了很迷的bug' ,name: '../htmls/blogs/001.html'},
         { title: '如何攥写一片高质量的数学建模论文' ,name: '../htmls/blogs/002.html'},
-        { title: '数学建模比赛基础了解' ,name: '../htmls/blogs/003.html'}
+        { title: '数学建模比赛基础了解' ,name: '../htmls/blogs/003.html'},
+        { title: '系统分析与设计(1)' ,name: '../htmls/blogs/004.html'}
       ]
     },
     methods: {
