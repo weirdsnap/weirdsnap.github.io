4e75bd18b2846d5e44d3b74597f970b70562cadc
dlb <dlb96@qq.com>
2018-03-13 15:55:16 +0800

    list modify

--- scripts/list.js
+++ scripts/list.js
@@ -5,7 +5,8 @@ var app4 = new Vue({
         { title: '刚开始写python遇到了很迷的bug' ,name: '../htmls/blogs/001.html'},
         { title: '如何攥写一片高质量的数学建模论文' ,name: '../htmls/blogs/002.html'},
         { title: '数学建模比赛基础了解' ,name: '../htmls/blogs/003.html'},
-        { title: '系统分析与设计(1)' ,name: '../htmls/blogs/004.html'}
+        { title: '系统分析与设计(1)' ,name: '../htmls/blogs/004.html'},
+        { title: 'temp' ,name: '../htmls/blogs/004.html'}
       ]
     },
     methods: {
